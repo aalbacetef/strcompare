973b203afdc9b61c984ce2705f6f9e70acf92b63
Arturo Albacete <aalbacetef@gmail.com>
2024-05-27 13:42:33 +0300

    implement damerau method; improve tests

--- src/levenshtein.zig
+++ src/levenshtein.zig
@@ -35,40 +35,48 @@ pub fn lev(alloc: std.mem.Allocator, a: []const u8, b: []const u8) !u64 {
     return mat.get(m, n);
 }
 
-fn head(s: []const u8) []const u8 {
-    if (s.len == 0) {
-        return "";
+pub fn damerau(alloc: std.mem.Allocator, a: []const u8, b: []const u8) !u64 {
+    const m = a.len;
+    const n = b.len;
+
+    const mat = try Matrix.init(alloc, m + 1, n + 1);
+    defer mat.deinit();
+
+    for (0..(m + 1)) |k| {
+        try mat.set(k, 0, k);
     }
 
-    return s[0..1];
-}
-
-fn tail(s: []const u8) []const u8 {
-    if (s.len < 2) {
-        return "";
+    for (0..(n + 1)) |k| {
+        try mat.set(0, k, k);
     }
 
-    return s[1..];
-}
+    for (1..(m + 1)) |i| {
+        for (1..(n + 1)) |j| {
+            var cost: u64 = 1;
+            if (a[i - 1] == b[j - 1]) {
+                cost = 0;
+            }
 
-test "it should return the first character" {
-    const v = head("asd");
-    try std.testing.expectEqualStrings("a", v);
-}
+            try mat.set(i, j, @min(
+                try mat.get(i - 1, j) + 1,
+                try mat.get(i, j - 1) + 1,
+                try mat.get(i - 1, j - 1) + cost,
+            ));
 
-test "it should return the first character if a single char" {
-    const v = head("a");
-    try std.testing.expectEqualStrings("a", v);
-}
+            const larger_than_1 = (i > 1) and (j > 1);
+            const in_bounds = (i < m) and (j < n);
+            const valid_index = larger_than_1 and in_bounds;
 
-test "it should return the tail of the string for multiple characters" {
-    const v = tail("asd");
-    try std.testing.expectEqualStrings("sd", v);
-}
+            if (valid_index and (a[i] == b[j - 1]) and (a[i - 1] == b[j])) {
+                try mat.set(i, j, @min(
+                    try mat.get(i, j),
+                    try mat.get(i - 2, j - 2) + 1,
+                ));
+            }
+        }
+    }
 
-test "it should return the tail of the string for single char" {
-    const v = tail("a");
-    try std.testing.expectEqual("", v);
+    return mat.get(m, n);
 }
 
 test "it should calculate correctly" {
@@ -76,7 +84,21 @@ test "it should calculate correctly" {
     const b = "asdafggaaccc";
 
     const want: u64 = 4;
-    const v = try lev(std.testing.allocator, a, b);
+    const v1 = try lev(std.testing.allocator, a, b);
+    const v2 = try damerau(std.testing.allocator, a, b);
 
-    try std.testing.expect(v == want);
+    try std.testing.expect(v1 == want);
+    try std.testing.expect(v2 == want);
+}
+
+test "it should detect strings of different lengths are different" {
+    const a = "asdffgghabv";
+    const b = "asdafggaaccc";
+
+    const v1 = try lev(std.testing.allocator, a, b);
+    const v2 = try damerau(std.testing.allocator, a, b);
+
+    const want: u64 = 5;
+    try std.testing.expect(v1 == want);
+    try std.testing.expect(v2 == want);
 }
