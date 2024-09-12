2e535dc66383a430d552daa4abb209e441922fcd
Arturo Albacete <aalbacetef@gmail.com>
2024-09-12 20:51:17 +0200

    fix: update path syntax in build.zig for v0.13

--- build.zig
+++ build.zig
@@ -5,14 +5,12 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     _ = b.addModule("strcompare", .{
-        .root_source_file = .{
-            .path = "./src/strcompare.zig",
-        },
+        .root_source_file = b.path("./src/strcompare.zig"),
     });
 
     const lib = b.addStaticLibrary(.{
         .name = "strcompare",
-        .root_source_file = .{ .path = "./src/strcompare.zig" },
+        .root_source_file = b.path("./src/strcompare.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -21,7 +19,7 @@ pub fn build(b: *std.Build) void {
 
     const test_step = b.step("test", "Run library tests");
     const t = b.addTest(.{
-        .root_source_file = .{ .path = "./src/strcompare.zig" },
+        .root_source_file = b.path("./src/strcompare.zig"),
         .target = target,
         .optimize = optimize,
     });
